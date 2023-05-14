2ebf847568c558a0cc2d5fab38997e2e72943943
m3vance <m3vance@gmail.com>
2023-05-14 12:08:56 -0700

    completed week 4 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,37 +15,88 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+describe("colorShuffle", () => {
+it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
-]
+const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+expect(["yellow", "blue", "pink", "green"]).toEqual(expect.arrayContaining(colorShuffle(colors1))),
+expect(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]).toEqual(expect.arrayContaining(colorShuffle(colors2)))
+    // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+  })
+})
+
+// output: ReferenceError: colorShuffle is not defined
 
 // b) Create the function that makes the test pass.
+  // pseudo code: 
+    // use .shift() method to remove the first value in the array
+    // find a method that randomizes the values in an array - .sort(() => Math.random() - 0.5)
+
+
+const colorShuffle = (array) => {
+  array.shift()
+  return array.sort(() => Math.random() - 0.5)
+}
+
+//output:  PASS  ./code-challenges.test.js
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("tallyCounter", () => {
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
+it("takes in an object that contains up votes and down votes and returns the end tally", () => {
+  expect(tallyCounter(votes1)).toEqual(11)
+  expect(tallyCounter(votes2)).toEqual(-31)
+  })
+})
+
+// output: ReferenceError: tallyCounter is not defined
 
 // b) Create the function that makes the test pass.
+  // pseudo code:
+    // use .notation to access the object values and subract them
+
+const tallyCounter = (object) => {
+  return object.upVotes - object.downVotes
+  }
+
+  // output:  PASS  ./code-challenges.test.js
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
+describe("combineNoDupez", () => {
 const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+it("takes in two arrays as arguments and returns one array with no duplicate values", () => {
+  expect(combineNoDupez(dataTypesArray1, dataTypesArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+  })
+})
+
+// output: ReferenceError: combineNoDupez is not defined
 
 // b) Create the function that makes the test pass.
+  //pseudo code: 
+    // function takes in dynamic number of arguments using ...
+    // create const variable combinedArray to hold a new Set of unique values
+    // combine new values with .flat() method
+    // convert the new combinedArray in to an array
+
+    const combineNoDupez = (...arrays) => {
+      const combinedArray = new Set(arrays.flat())
+      return [...combinedArray]
+    }
+
+// output:  PASS  ./code-challenges.test.js
+
+// console.log(combineNoDupez(dataTypesArray1, dataTypesArray2));
